37ab28e4477cce42d64b4834cf87fe8fd4f081a6
Ben Caine <bcaine0@gmail.com>
2015-09-20 18:37:10 -0400

    Added accumulator code for accu_10

---
+++ shared/accu_10.v
@@ -0,0 +1,16 @@
+module accu10(out, overflow, in, clk, rst);
+	// Module to take a single bit input and turn it into a 10 bit output
+	input in;
+	input clk;
+	input rst;
+	output wire [9:0] out;
+	output reg overflow;
+	always@(posedge clk or negedge rst) begin
+		if(~rst) begin
+			out <= 'b0;
+			overflow <= 'b0;
+		end else begin
+			{overflow, out} <= out + A;
+		end
+	end
+endmodule
