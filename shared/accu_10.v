b119a50a09609b4a19e5709602d68bd39f82908e
Ben Caine <bcaine0@gmail.com>
2015-09-20 20:46:35 -0400

    Fixed misnamed variable and added counter

--- shared/accu_10.v
+++ shared/accu_10.v
@@ -3,14 +3,17 @@ module accu10(out, overflow, in, clk, rst);
 	input in;
 	input clk;
 	input rst;
+	reg [3:0] counter;
 	output wire [9:0] out;
 	output reg overflow;
 	always@(posedge clk or negedge rst) begin
-		if(rst) begin
+		if(rst or counter > 10) begin
+			counter <= 'b0;
 			out <= 'b0;
 			overflow <= 'b0;
 		end else begin
-			{overflow, out} <= out + A;
+			{overflow, out} <= out + in;
+			counter <= counter + 1
 		end
 	end
 endmodule
